3e75d91e05868d78fe0f1c14adbe37575c39577b
Eclixal <alexandre.jousset@elesia.org>
2019-05-29 11:03:37 +0200

    PATCH build.XML afin de permettre l'execution du JAR

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,3 +1,10 @@
+/**
+ *
+ * @author Alex LOISEL
+ * @author Alexandre JOUSSET
+ * @author Titouan LOISEL
+ * @author Erwann COTHENET
+ */
 public class Launcher {
 
     public static void main(String[] args) {
