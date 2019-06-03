872a9797f674ef94cf7c12cfe34e5c046c0884cf
griffin568 <messenger133@laposte.net>
2019-06-03 09:13:45 +0200

    javadoc classe Partie

--- src/quoridor/Partie.java
+++ src/quoridor/Partie.java
@@ -2,6 +2,11 @@ package quoridor;
 
 import java.util.ArrayList;
 
+/**
+  *Cette classe gère les différents aspect de la partie
+  *@author AlexM02 , Drmarsupial35 , Eclixal , griffin568
+  *@version 0.1.0
+  */
 public class Partie {
 
     private int tour;
@@ -9,38 +14,70 @@ public class Partie {
     private Plateau plateau;
     private ArrayList<Joueur> joueur;
 
+    /**
+      *Créé un nouvel objet Partie
+      *@param fileName le nom du fichier de configuration
+      */
     public Partie(String fileName) {
 
     }
 
-    public int getTour() {
+  /**
+    *Retourne le numéro du tour actuel
+    *@return le numéro du tour
+    */
+  public int getTour() {
         return tour;
     }
 
+    /**
+      *Retourne le mode de jeu utilisé
+      *@return le mode de jeu utilisé
+      */
     public Mode getMode() {
         return mode;
     }
 
+    /**
+      *Sauvegarde la partie
+      */
     public void sauvegarder() {
 
     }
 
+    /**
+      *Charge les données de sauvegarde contenues dans le fichier sélectionné
+      *@param filename le fichier contenant les données à charger
+      */
     public void charger(String filename) {
 
     }
 
+    /**
+      *Initialise les différents éléments constants de la partie
+      */
     private void initialisation() {
 
     }
 
+    /**
+      *Configure les éléments non constants de la partie à l'aide du fichier de configuration
+      *@param filename le nom du fichier de configuration
+      */
     private void configuration(String filename) {
 
     }
 
+    /**
+      *Lance la partie
+      */
     public void start() {
 
     }
 
+    /**
+      *Termine la partie
+      */
     public void fin() {
 
     }
