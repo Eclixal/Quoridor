47bc9495cef8b2f81a0a14caa59c8d2da0435aa3
griffin568 <messenger133@laposte.net>
2019-06-03 08:17:58 +0200

    javadoc classe Difficulte

--- src/quoridor/Difficulte.java
+++ src/quoridor/Difficulte.java
@@ -1,5 +1,10 @@
 package quoridor;
 
+/**
+  *Enumeration contenant les differents niveaux de difficulte de l'IA
+  *@author AlexM02 , Drmarsupial35 , Eclixal , griffin568
+  *@version 1.0
+  */
 public enum Difficulte {
 
     FACILE,
