6a2d829c39d5762cef2b31e51792034907426f58
Eclixal <alexandre.jousset@elesia.org>
2019-06-03 09:21:40 +0200

    PlateauTest

--- src/test/PlateauTest.java
+++ src/test/PlateauTest.java
@@ -1,12 +1,16 @@
 package test;
 
 import org.junit.Test;
+import quoridor.Plateau;
+
+import static org.junit.Assert.assertEquals;
 
 public class PlateauTest {
 
     @Test()
     public void testPlateau() {
-
+        Plateau plateau = new Plateau(10);
+        assertEquals(10, plateau.getTaille());
     }
 
 }
