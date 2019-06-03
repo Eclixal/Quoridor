e69e83b6728209524a86252f24b6778720f6a03d
griffin568 <messenger133@laposte.net>
2019-06-03 09:26:26 +0200

    javadoc Launcher

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,7 @@
 /**
- *
- * @author Alex LOISEL
- * @author Alexandre JOUSSET
- * @author Titouan LOISEL
- * @author Erwann COTHENET
- */
+  *Cette classe permet de lancer l'application
+  *@author AlexM02 , Drmarsupial35 , Eclixal , griffin568
+  */
 public class Launcher {
 
     public static void main(String[] args) {
